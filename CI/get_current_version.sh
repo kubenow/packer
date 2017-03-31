c4014c4f341bab55baf564f772a4816c4d6ec3d9
carmat88 <matteo.carone@farmbio.uu.se>
2017-03-31 13:43:11 +0200

    Improving CI file 26

---
+++ CI/get_current_version.sh
@@ -0,0 +1,18 @@
+#get_current_version.sh v1
+#Author: carmat88
+
+#!/bin/bash 
+
+#Get current_version
+export current_version=v$(git describe --tags --always | tr -d .)
+
+regex=^v[0-9]{3}([ab][0-9]{1,}|rc[0-9]{1,})?$
+
+#Checking whether or not current version is tagged as release
+if [[ $current_version =~ $regex ]]; then 
+	echo "Current version tagged as release"
+else 
+	export current_version="current"
+fi
+
+echo "Current version is: $current_version"
