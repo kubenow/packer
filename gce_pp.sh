37c11a5278229a2ff48bb78f78a7141c24864f8c
carmat88 <matteo.carone@farmbio.uu.se>
2017-04-25 14:49:50 +0200

    Improving CI file 49

--- gce_pp.sh
+++ gce_pp.sh
@@ -3,19 +3,24 @@
 # This Script will be executed during the post-processor of the GCE packer builder
 # It downloads the compressed image just created, extracts it and converts to a QCOW format (to be used for OpenStack providers)
 
+#Sleeping so to wait that image in Google Bucket is ready
+sleep 3m
+
+#Simple checking that image is ready
+gsutil ls gs://kubenow-images/
 
 #Donwloading kubenow-current compressed image from Google Storage
-echo "\nDownloading kubenow compressed image from Google bucket..."
+echo "Downloading kubenow compressed image from Google bucket..."
 gsutil cp gs://kubenow-images/kubenow-current.tar.gz .
 
 #Extracting it
-echo '\nExtracting image tar...'
+echo "Extracting image tar..."
 tar -xvzf kubenow-current.tar.gz
 
 #Converting image from raw to qcow format
-echo '\nConverting RAW image into QCOW2 format...'
+echo "Converting RAW image into QCOW2 format..."
 qemu-img convert -f raw -O qcow2 disk.raw kubenow-current.qcow2
 
 #Uploading the new image format back to the Google Storage bucket
-echo '\nUploading new image format back into the Google buket...'
+echo "Uploading new image format back into the Google buket..."
 gsutil cp -a public-read kubenow-current.qcow2 gs://kubenow-images/
